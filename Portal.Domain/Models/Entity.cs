12a97328264cb196475b716b3d809573a688992a
maxpayne301 <pretteviski@hotmail.com>
2024-09-09 23:45:43 -0300

    structuring project

---
+++ Portal.Domain/Models/Entity.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Portal.Domain.Models;
+
+public class Entity
+{
+
+}
---
+++ Portal.Infra/AppContext.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portal.Infra;
+
+public class AppContext : DbContext
+{
+
+}
